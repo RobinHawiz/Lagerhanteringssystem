d19d536c7b199ae828cacb5a32ed554ec6b3f751
RobinHawiz <Robin.Hawiz.6670@student.uu.se>
2026-03-12 16:59:22 +0100

    Protect Employees CRUD with admin role and allow public read

--- MVC/Controllers/EmployeesController.cs
+++ MVC/Controllers/EmployeesController.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MVC.Data;
 using MVC.Models.Entities;
 
 namespace MVC.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class EmployeesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -20,12 +17,14 @@ namespace MVC.Controllers
         }
 
         // GET: Employees
+        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
             return View(await _context.Employees.ToListAsync());
         }
 
         // GET: Employees/Details/5
+        [AllowAnonymous]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
