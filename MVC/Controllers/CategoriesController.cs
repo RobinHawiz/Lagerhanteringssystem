fa057cf1195e85e57ea3fae60957a6e6263a6ca7
RobinHawiz <Robin.Hawiz.6670@student.uu.se>
2026-03-13 15:44:06 +0100

    restrict categories to admin and add nav link

--- MVC/Controllers/CategoriesController.cs
+++ MVC/Controllers/CategoriesController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MVC.Data;
 using MVC.Models.Entities;
 
 namespace MVC.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _context;
